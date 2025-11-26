e7e5aea781a1b612dd615b3a986dfb990e445c6c
nupoor kedare <nupoorktech@gmail.com>
2025-11-26 14:32:15 +0530

    Update test_plbasic.py

--- test_plbasic.py
+++ test_plbasic.py
@@ -1,23 +1,28 @@
-import re
-from playwright.sync_api import Page, expect
-
 
 def test_playwrightBasic(playwright):
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
-    page.goto("https://playwright.dev/")
+    page.goto("https://playwright.dev/")   
+    
+#page.goto() = “open this URL in the current tab."
+# Think of page as:
+# A tab inside a browser that you can control through code
 
+# A basic Python Playwright test looks like:
 
-#chromium headless mode 1. single context
-# def test_playwrightShortCut(page: Page):
-#     page.goto("https://playwright.dev/")
+# def test_example(page):
+#     page.goto("https://example.com")
+#     page.click("text=Login")
 
-def test_get_started_link(page: Page):
-    page.goto("https://playwright.dev/")
+# Let’s break it gently:
 
-    # Click the get started link.
-    page.get_by_role("link", name="Get started").click()
+# def test_example(page):
+# → Playwright gives you the page object automatically using a fixture.
 
-    # Expects page to have a heading with the name of Installation.
-    expect(page.get_by_role("heading", name="Installation")).to_be_visible()
+# page.goto(...)
+# → Open the website.
+
+# page.click(...)
+# → Interact with something.
+
